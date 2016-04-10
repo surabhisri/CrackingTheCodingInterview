0558d23042f7b8e391357d5c93b9431d291bca88
Surabhi Srivastava <srivastavasurabhi4@gmail.com>
2016-04-09 20:48:49 -0700

    Initial Structure for Problem 1.4
    This is the first skeleton for the C# implementation

---
+++ C#/Chapter1/1.4/ReplaceSpaces.cs
@@ -0,0 +1,33 @@
+/*	Program: Replace spaces in an input string with %20 
+ *	Author: Surabhi Srivastava
+ *	
+ *	Date: 9 April, 2016
+ */
+
+using System;
+
+namespace CrackingTheCodingInterview
+{
+    /// <summary>
+    /// This class has methods to replace spaces in input string with %20
+    /// </summary>
+    public class ReplaceSpace
+    {
+
+    }
+
+    /// <summary>
+    /// This class tests the method in ReplaceSpace class
+    /// </summary>
+    internal class ReplaceSpaceTest
+    {
+        /// <summary>
+        /// Program execution starts here
+        /// </summary>
+        static void Main()
+        {
+            Console.WriteLine("Hello");
+            Console.ReadLine();
+        }
+    }
+}
